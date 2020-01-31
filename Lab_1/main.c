3c342bc03605217c839e52dfd87db715e0e75044
Mark Mersnik <markmersnik@gmail.com>
2020-01-30 17:47:47 -0800

    Setup environment and turned on display.

---
+++ Lab_1/main.c
@@ -0,0 +1,15 @@
+#include "msp.h"
+
+
+/**
+ * main.c
+ */
+void main(void)
+{
+    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
+    P8->DIR = 0xff;
+    P4->DIR = 0xff;
+    P8->OUT |= (BIT4 | BIT3| BIT2);
+    P8->OUT &= ~BIT5;
+    P4->OUT = ~0x01;
+}
