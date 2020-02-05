4a198111b88b2f45c20ec7e83005e7a05c0060c7
Mark Mersnik <markmersnik@gmail.com>
2020-02-05 15:02:37 -0800

    keypad implemented but display communication needs work.

--- Lab_1/main.c
+++ Lab_1/main.c
@@ -4,6 +4,20 @@
  * main.c
  */
 
+unsigned int d = 0;
+unsigned int row = 0;
+unsigned int col = 0;
+unsigned int nums[] = {0x00, 0x00, 0x00, 0x00};
+unsigned int displays[] = {BIT5, BIT4, BIT3, BIT2};
+unsigned int rows[] = {BIT5, BIT4, BIT3, BIT2};
+unsigned int cols[] = {BIT3, BIT2, BIT1, BIT0};
+unsigned int keys[4][4] = {
+                         {0x06, 0x5B, 0x4F, 0x77},
+                         {0x66, 0x6D, 0x7D, 0x7C},
+                         {0x07, 0x7F, 0x67, 0x39},
+                         {0x00, 0x3F, 0x00, 0x5E}
+};
+
 void waitTime(unsigned int time){
     unsigned int i = 0;
     while(i < time){
@@ -39,55 +53,124 @@ void display_numbers(){
     }
 }
 
-void display_key(char key) {
-    unsigned int displays[] = {BIT5, BIT4, BIT3, BIT2};
-    //unsigned int i = 0;
-    switch(key) {
-        case '1':
-            P8->OUT |= (BIT5 | BIT4 | BIT3| BIT2);
-            P4->OUT = ~0x06;
-            //P8->OUT &= ~displays[i];
+void release_debouncing() {
+    int count = 0;
+    P8->OUT |= (BIT5 | BIT4 | BIT3| BIT2);
+    P8->DIR |= rows[row];
+    P8->OUT &= ~rows[row];
+    while(count < 10) {
+        if(count == 10){
             break;
-        case '2' :
-            P8->OUT |= (BIT5 | BIT4 | BIT3| BIT2);
-            P4->OUT = ~0x5B;
-            P8->OUT &= ~displays[1];
+        }
+        else if(P9->IN & cols[col]) {
+            count = 0;
+        }
+        else if (P9->IN & ~cols[col]){
+            count++;
+        }
+    }
+}
+
+void display_key() {
+
+    //unsigned int time = 0;
+    if(d > 3){
+        d = 0;
+    }
+
+    //Number 1
+    P8->OUT |= (BIT5 | BIT4 | BIT3| BIT2);
+    P4->OUT = nums[0];
+    P8->OUT &= ~displays[0];
+    waitTime(10);
+
+    //Number 2
+    P8->OUT |= (BIT5 | BIT4 | BIT3| BIT2);
+    P4->OUT = nums[1];
+    P8->OUT &= ~displays[1];
+    waitTime(10);
+
+    //Number 3
+    P8->OUT |= (BIT5 | BIT4 | BIT3| BIT2);
+    P4->OUT = nums[2];
+    P8->OUT &= ~displays[2];
+    waitTime(10);
+
+    //Number 4
+    P8->OUT |= (BIT5 | BIT4 | BIT3| BIT2);
+    P4->OUT = nums[3];
+    P8->OUT &= ~displays[3];
+    waitTime(10);
+
+    release_debouncing();
+
+//    switch(key) {
+//        case '1':
+//            P8->OUT |= (BIT5 | BIT4 | BIT3| BIT2);
+//            P4->OUT = ~0x06;
+//            //P8->OUT &= ~displays[i];
+//            break;
+//        case '2' :
+//            P8->OUT |= (BIT5 | BIT4 | BIT3| BIT2);
+//            P4->OUT = ~0x5B;
+//            P8->OUT &= ~displays[1];
+//            break;
+//        case '3' :
+//            P8->OUT |= (BIT5 | BIT4 | BIT3| BIT2);
+//            P4->OUT = ~0x4F;
+//            P8->OUT &= ~displays[2];
+//            break;
+//        case 'A' :
+//            P8->OUT |= (BIT5 | BIT4 | BIT3| BIT2);
+//            P4->OUT = 0x88;
+//            P8->OUT &= ~displays[3];
+//            break;
+//    }
+}
+
+void press_debouncing() {
+    int count = 0;
+    while(count < 11) {
+        if(count == 10){
+            nums[d] = ~keys[row][col];
+            display_key();
             break;
-        case '3' :
-            P8->OUT |= (BIT5 | BIT4 | BIT3| BIT2);
-            P4->OUT = ~0x4F;
-            P8->OUT &= ~displays[2];
+        }
+        else if(P9->IN & cols[col]) {
+            count++;
+        }
+        else if (P9->IN & ~cols[col]){
+            count = 0;
             break;
-        case 'A' :
-            P8->OUT |= (BIT5 | BIT4 | BIT3| BIT2);
-            P4->OUT = 0x88;
-            P8->OUT &= ~displays[3];
-            break;
+        }
     }
 }
 
 void scan(){
     unsigned int k = 0;
-    unsigned int cols[] = {BIT3, BIT2, BIT1, BIT0};
 
-    P8->DIR |= BIT5;
     P9->DIR &= ~BIT3;
-    P8->OUT &= ~BIT5;
     P9->OUT &= ~BIT3;
 
+
     while(1){
-        if(P9->IN & cols[k]) {
-            switch(k) {
-                case(0): display_key('1');
-                case(1): display_key('2');
-                case(2): display_key('3');
-                case(3): display_key('A');
+        P8->OUT |= (BIT5 | BIT4 | BIT3| BIT2);
+        P8->DIR |= rows[k];
+        P8->OUT &= ~rows[k];
+        int i;
+        for(i = 0; i < 4; i++){
+            if(P9->IN & cols[i]) {
+                row = k;
+                col = i;
+                press_debouncing();
+                d++;
             }
         }
         k = k + 1;
         if(k == 4){
-            k = 0;
+           k = 0;
         }
+        waitTime(10);
     }
 }
 
@@ -98,11 +181,9 @@ void main(void)
     P4->DIR = 0xff;
 
     P8->OUT |= (BIT5 | BIT4 | BIT3| BIT2);
-    P4->OUT = 0xC0; //Adjusts what turns on.
+    P4->OUT = 0xff;
     P8->OUT &= ~BIT5;
-
     scan();
-
 }
 
 
